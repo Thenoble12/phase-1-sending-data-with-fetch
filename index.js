2339a8811fac6433db35ab5401c831a21265bd9e
Elijah Noble <elijahnobledev@gmail.com>
2022-05-08 19:11:33 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+const destinationURL = "http://localhost:3000/users"
+
+function submitData(userName, userEmail) {
+    let formData = {
+        name: userName,
+        email: userEmail,
+    }
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(formData), 
+};
+
+return fetch(destinationURL, configurationObject)
+    .then(response => response.json())
+    .then(object => {
+        let p = document.createElement('p');
+        p.innerHTML = `ID: ${object.id}, NAME: ${object.name}, EMAIL: ${object.email}`;
+        document.body.appendChild(p);        
+    })
+    .catch(error => {
+        alert("Error!");
+        let p = document.createElement('p');
+        p.innerHTML = `ERROR: ${error.message}`
+        document.body.appendChild(p);
+    });
+
+}
